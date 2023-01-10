20e56d80f4f39ffc867c74709ca0b5cb4f820efc
Spencer <spencerb242@proton.me>
2023-01-09 20:42:07 -0700

    added login form and signup form with styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//page components
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,45 @@
 import styles from "./Signup.module.css";
 
+import { useState } from "react";
+
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, username);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Signup</h2>
+      <label>
+        <span>username:</span>
+        <input
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }
