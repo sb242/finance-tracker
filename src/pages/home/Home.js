296131c30271bc633f0b59a09cfcfa2e4711983c
Spencer <spencerb242@proton.me>
2023-01-10 15:52:17 -0700

    created transaction form component and mounted it inside the Home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,15 @@
 import styles from "./Home.module.css";
 
+//components
+import TransactionForm from "./TransactionForm";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
