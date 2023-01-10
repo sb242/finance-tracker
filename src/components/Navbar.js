42cd5223d13fa01aab843a559ed2f0b6e40d5856
Spencer <spencerb242@proton.me>
2023-01-10 14:52:16 -0700

    implemented useLogin hook and now conditionally renders display name and other elements in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
-  const { error, isPending, logout } = useLogout();
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
