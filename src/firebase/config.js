d63d0a79bdd35becc5d9e91de60377a2dfd0ed09
Spencer <spencerb242@proton.me>
2023-01-09 20:49:58 -0700

    setup firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhn4qbQuYk7Q1KrHWYw-JSbiFRECUVSxk",
+  authDomain: "mymoney-cbedf.firebaseapp.com",
+  projectId: "mymoney-cbedf",
+  storageBucket: "mymoney-cbedf.appspot.com",
+  messagingSenderId: "624214955221",
+  appId: "1:624214955221:web:e9f687d868f7fd15833e5c",
+};
+
+//init firebase
+
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -5,22 +5,22 @@ import { useState } from "react";
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+  const [displayName, setDisplayName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, username);
+    console.log(email, password, displayName);
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles["signup-form"]}>
       <h2>Signup</h2>
       <label>
-        <span>username:</span>
+        <span>display name:</span>
         <input
           type="text"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
         />
       </label>
       <label>
