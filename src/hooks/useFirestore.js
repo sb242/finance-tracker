d0981316ae3e9053ef68176366ec9b2a8345e166
Spencer <spencerb242@proton.me>
2023-01-11 11:45:29 -0700

    added an element button for the user to click on to delete a transaction, updates in firestore collection as well

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -24,6 +24,13 @@ const firestoreReducer = (state, action) => {
         success: true,
         error: null,
       };
+    case "DELETED_DOCUMENT":
+      return {
+        isPending: false,
+        document: null,
+        success: true,
+        error: null,
+      };
     case "ERROR":
       return {
         isPending: false,
@@ -67,7 +74,18 @@ export const useFirestore = (collection) => {
   };
 
   //delete document
-  const deleteDocument = async (id) => {};
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await ref.doc(id).delete();
+      dispatchIfNotCancelled({
+        type: "DELETED_DOCUMENT",
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: "could not delete" });
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
