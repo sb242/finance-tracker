27843606154156aa89453995ea1c6a57fa89f761
Spencer <spencerb242@proton.me>
2023-01-11 11:14:40 -0700

    called a firestore query ontransactions collection to filter to only give what transactions are for that current logged in user.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,25 @@
 import styles from "./Home.module.css";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
 //components
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions", [
+    "uid",
+    "==",
+    user.uid,
+  ]);
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
